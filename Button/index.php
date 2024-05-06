50b74473ab16a6fd221a6659624dbbeaa21f1fcb
Faisal-Muhamad-Fahlevi <faisalmfahlevi@gmail.com>
2024-05-06 20:54:08 +0700

    Latihan Button

---
+++ Button/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS Card</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-img">
+                <img src="https://imgcdn.oto.com/large/gallery/exterior/7/2234/daihatsu-sigra-front-angle-low-view-282430.jpg" alt="">
+            </div>
+
+            <div class="card-body">
+                <h2 class="card-title">Drive (2011)</h2>
+                <p class="card-intro"> 
+                    Driver is a skilled Hollywood stuntman who moonlights as a getaway driv...
+                </p>
+        </div>
+
+    </div>
+</div>
+    
+</body>
+</html>
